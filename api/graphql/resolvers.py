261769416a7ca3d7861ea513ce4a3e815ff1b497
clbj <clovesbarcellosjr@gmail.com>
2021-01-24 02:52:23 +0000

    Jwt schema setup

--- api/graphql/resolvers.py
+++ api/graphql/resolvers.py
@@ -1,12 +1,14 @@
 from ariadne import QueryType, make_executable_schema, load_schema_from_path, MutationType
 from ariadne_jwt import resolve_verify, resolve_refresh, resolve_token_auth, jwt_schema, GenericScalar
+from ariadne_jwt.decorators import login_required
 from api.models.user import User
 from api.models.audit import Audit
 
 
 type_defs = [
     load_schema_from_path("api/graphql/schema.graphql"),
-    #load_schema_from_path("api/graphql/characters.graphql"),
+    load_schema_from_path("api/graphql/jwt.graphql"),
+    load_schema_from_path("api/graphql/characters.graphql"),
     #load_schema_from_path("api/graphql/comics.graphql")
 ]
 
@@ -17,14 +19,6 @@ mutation.set_field('verifyToken', resolve_verify)
 mutation.set_field('refreshToken', resolve_refresh)
 mutation.set_field('tokenAuth', resolve_token_auth)
 
-@query.field('users')
-def resolve_users(*_):
-    return User.objects.all()
 
 
-@query.field('audit')
-def resolve_users(*_):
-    return Audit.objects.all()
-
-
-schema = make_executable_schema(type_defs,jwt_schema,query,mutation,GenericScalar)
+schema = make_executable_schema(type_defs,query,mutation,GenericScalar)
